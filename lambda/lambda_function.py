0493e27eb592b8db9c830b2058717fb4693318d4
lyndonteng <lyndon@live.com.sg>
2020-01-19 01:44:27 +0000

    Update lambda_function.py

--- lambda/lambda_function.py
+++ lambda/lambda_function.py
@@ -2,4 +2,4 @@ from googletrans import Translator
 translator = Translator()
 
 ### put this in the logic for handling input.
-translator.translate(input_string, dest='en')
+result = translator.translate(input_string, dest='en')
