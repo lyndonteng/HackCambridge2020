375d7d755449d5f574706c850529e74086d1ada6
lyndonteng <lyndon@live.com.sg>
2020-01-19 01:33:50 +0000

    upload google cloud to github

---
+++ Microsoft/duolingo.py
@@ -0,0 +1,12 @@
+import requests
+
+requests.post("transcribestreaming.region.amazonaws.com")
+authorization: Generated value
+content-type: application/vnd.amazon.eventstream
+x-amz-target: com.amazonaws.transcribe.Transcribe.StartStreamTranscription
+x-amz-content-sha256: STREAMING-AWS4-HMAC-SHA256-EVENTS
+x-amz-date: Date
+x-amzn-transcribe-language-code: fr-FR
+x-amzn-transcribe-media-encoding: pcm
+x-amzn-transcribe-sample-rate: 8000
+transfer-encoding: chunked
