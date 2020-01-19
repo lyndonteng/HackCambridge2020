7e326197dfb7fb75030bb75586c3398000b6eb1c
lyndonteng <lyndon@live.com.sg>
2020-01-19 11:20:45 +0000

    Create data.py

---
+++ lambda/data.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+
+TOPICS = ['weather', 'time', 'place']
+
+#WELCOME_MESSAGE = ('<speak>This is english. <voice name="Celine"><lang xml:lang="fr-FR">Bonjour madame. Le ciel est très bleu.</lang></voice></speak>')
+WELCOME_MESSAGE = '<amazon:emotion name="excited" intensity="high">Welcome to chatterbox. Nice to see you again! What did you learn in your last french lesson? Remember, reply only in french.</amazon:emotion>'
+
+REVISE_TOPIC_MESSAGE = 'Great! I have a range of similar topics. Would you like to start a lesson on one of them?'
+
+LISTENING_PASSAGE_MESSAGE = 'Lets start with a story about how Mariah starts her day. I will ask you a few questions afterwards.'
+
+LISTENING_PASSAGE = ['Mariah se réveille régulièrement à 6 heures du matin. Elle va courir, puis elle prend une douche et met les vêtements qu\'elle a préparés le soir précédente. Elle va à la salle de baine pour se brosser les dents et se maquiller, ce qui est plus difficile depuis que les lumières des miroirs se sont cassées il y a une semaine. Elle a généralement des céréales avec des fruits pour le petit déjeuner et part travailler vers 7h30.']
+
+LISTENING_PASSAGE_QUESTION_START = 'Here is your question.'
+
+QUESTION_LIST = ['Quand mariah se douche-t-elle habituellement?', 'Ce qui est cassé dans la salle de bain?', 'De quelle couleur est sa chemise?']
+
+sayinfrench_QUESTION_LIST = ['bathroom', 'lights', 'clothes']
+sayinfrench_ANSWER_LIST = ['la salle de baine', 'les lumières', 'les vêtements']
+
+ANSWER_LIST = ['dans la matinée', 'la toilette', 'rouge']
+
+EXIT_SKILL_MESSAGE = 'It was fun revising French with You! See you soon!'
+
+################################# OUR STUFF ^^^^^^^ #############################################
+
+IMG_PATH="https://images.fineartamerica.com/images/artworkimages/mediumlarge/1/8-french-flag-les-cunliffe.jpg"
